f7b4a41c0aeab0b53e588651f6f17a4dc8c3030f
Muhammed <47753807+muhammeddeniz@users.noreply.github.com>
2019-07-06 10:56:14 +0300

    Add files via upload

---
+++ Örnek_1/örnek_1.py
@@ -0,0 +1,10 @@
+""""
+Kullanıcı tarafından girilen iki sayının toplamını veren program
+
+"""
+
+
+birinciSayi = input("birinci sayiyi giriniz: ")
+ikinciSayi = input("ikinci sayiyi giriniz: ")
+
+print("iki sayinin toplami = ", int(birinciSayi) + int(ikinciSayi))
